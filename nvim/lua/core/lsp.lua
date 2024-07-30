f1c9caa31dae7acd60aa3302adff449b8e822222
Tim Vick <t.vick@yahoo.com>
2024-07-30 08:37:11 -0700

    fix: adds mapping for code_action

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -20,6 +20,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
     vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end
 })
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,33 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Tokyo Night (Gogh)'
+config.font_size = 16.0
+config.line_height = 1.2
+
+config.window_decorations = "RESIZE"
+
+config.window_padding = {
+  left = 5,
+  right = 5,
+  bottom = 0,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 
 config.font =
   -- wezterm.font('JetBrainsMono Nerd Font', {weight='Light'})
   wezterm.font('Berkeley Mono', {weight='Regular'})
 
+config.window_background_gradient = {
+  colors = { '#12100E', '#1B2845' },
+  -- Specifices a Linear gradient starting in the top left corner.
+  orientation = { Linear = { angle = -45.0 } },
+}
+
+return config
